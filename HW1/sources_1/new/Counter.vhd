15b47ff134a660545d7fcfa6d7f684d048ada420
CCH147 <08220822max@gmail.com>
2024-10-16 16:36:18 +0800

    Update Counter.vhd

--- HW1/sources_1/new/Counter.vhd
+++ HW1/sources_1/new/Counter.vhd
@@ -8,8 +8,7 @@ entity Counter is
         Clk                      : in  std_logic;
         nRst                     : in  std_logic; -- 負緣
         up_counter              : out std_logic_vector(3 downto 0); -- 4-bit 
-        down_counter              : out std_logic_vector(3 downto 0); -- 4-bit 
-        st                       :out std_logic
+        down_counter              : out std_logic_vector(3 downto 0) -- 4-bit 
         );
 end entity;
   
@@ -17,25 +16,33 @@ architecture rtl of Counter is --rtl 動作定義
     
     signal counter_up: std_logic_vector(3 downto 0); --定義 4-bit 訊號
     signal counter_down: std_logic_vector(3 downto 0); --定義 4-bit 訊號
-    signal divclk    : std_logic_vector(25 downto 0);
-    signal fclk      : std_logic_vector(25 downto 0);
+    signal divclk    : std_logic_vector(26 downto 0);
+	signal fclk                  : std_logic;                    --定義 除頻 clk
     signal state : std_logic;
     begin
     
+    FD:process(Clk,Rst)    --除頻器
+    begin
+        if (nRst = '1') then
+            divclk <= (others=>'0');
+        elsif (rising_edge(Clk)) then
+            divclk <= divclk + 1;
+        end if;
+    end process FD;  
+    fclk <= divclk(24); --約 2 Hz
    --process(Clk)
    -- begin
    -- divclk <= divclk + 1;
    -- end process;
    -- fclk <= divclk;
     up_counter <= counter_up; 
-    down_counter <= counter_down; 
-    st <= state;
-  
-    process(Clk)
+	down_counter <= counter_down;
+	
+    process(fclk)
     begin
         if (nRst = '1') then
             counter_down <= "1000"; --重置
-        elsif Clk'event and Clk = '1' then 
+        elsif fclk'event and fclk = '1' then 
             case state is
                 when '0'=>
                     counter_down <= "1000";
@@ -50,11 +57,11 @@ architecture rtl of Counter is --rtl 動作定義
             
     end process;
      
-    process(Clk)
+    process(fclk)
     begin
         if (nRst = '1') then
             counter_up <= "0010";
-        elsif Clk'event and Clk = '1' then 
+        elsif fclk'event and fclk = '1' then 
             case state is
                 when '0'=>
                     if(counter_up < "1000") then
